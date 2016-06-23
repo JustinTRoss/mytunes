71239e9b144d3bc2b515f0e920fdb266264dd62b
JustinTRoss <ross.justin.t@gmail.com>
2016-06-22 20:59:48 -0700

    Complete enqueue, dequeue, play, ended, songQueue, etc

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,25 @@ var SongQueue = Backbone.Collection.extend({
   model: SongModel,
 
   initialize: function() {
+    this.on('add', function(song) {
+      if (this.length <= 1) {
+        this.at(0).play();
+      }
+    });
+
+    this.on('remove', function() {
+      if (this.length > 0) {
+        this.at(0).play();
+      }
+    });
+
+    this.on('ended', function() {
+      this.remove(this.at(0));
+    });
+
+
   }
+
+
 
 });
--- client/views/SongQueueView.js
+++ client/views/SongQueueView.js
@@ -1,12 +1,34 @@
-// SongQueueView.js - Defines a backbone view class for the song queue.
+// SongQueueView.js - Defines a backbone view class for the song queue. LIST OF SONGS IN THE QUEUE
 var SongQueueView = Backbone.View.extend({
 
+  tagName: 'table',
+
   initialize: function() {
+    this.render();
+
   },
 
   render: function() {
-  initialize: function() {
-  },
+    // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
+    // see http://api.jquery.com/detach/
+    this.$el.children().detach();
+
+    this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song) {
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
   }
+
+
+
+
+  // initialize: function() {
+  // },
+
+  // render: function() {
+  // initialize: function() {
+  // },
+  // }
 
 });
